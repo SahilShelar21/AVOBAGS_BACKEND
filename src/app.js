fdf5c75e80c58b47dabe13397b123405545724d9
Sahil Vijay shelar <shelarsahil121@gmail.com>
2026-02-13 03:50:20 +0530

    frontend web change

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://avobags-frontend.onrender.com"
+    "https://avobags-frontend-v1.onrender.com"
   ],
   credentials: true
 }));
